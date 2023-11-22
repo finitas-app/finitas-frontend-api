7608b3656c069a3802b1c0b394d70b91a5b48aed
Danylo Hrebeniuk <84989172+daniilgrebenuk@users.noreply.github.com>
2023-11-22 21:18:02 +0100

    Generalize external error response (#4)
    Co-authored-by: Danylo Hrebeniuk <dhr@touk.pl>

--- src/main/kotlin/com/finitas/config/HttpClient.kt
+++ src/main/kotlin/com/finitas/config/HttpClient.kt
@@ -1,12 +1,26 @@
 package com.finitas.config
 
+import com.finitas.config.exceptions.ErrorResponse
+import com.finitas.config.exceptions.ExternalErrorException
 import io.ktor.client.*
+import io.ktor.client.call.*
 import io.ktor.client.engine.cio.*
+import io.ktor.client.plugins.*
 import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.serialization.kotlinx.json.*
 
-val client = HttpClient(CIO) {
+
+val httpClient: HttpClient = HttpClient(CIO) {
     install(ContentNegotiation) {
         json()
+    }
+    HttpResponseValidator {
+        validateResponse { response ->
+            val httpStatusCode = response.status
+            if (httpStatusCode.value in 400..599) {
+                val error = response.body<ErrorResponse>()
+                throw ExternalErrorException(error, httpStatusCode)
+            }
+        }
     }
 }
--- src/main/kotlin/com/finitas/config/exceptions/errorDtos.kt
+++ src/main/kotlin/com/finitas/config/exceptions/errorDtos.kt
@@ -12,4 +12,6 @@ enum class ErrorCode {
     NOT_FOUND,
     GENERIC_ERROR,
     NO_FILE_PROVIDED,
+    FILE_NOT_PROVIDED,
+    INVALID_FILE_PROVIDED,
 }
