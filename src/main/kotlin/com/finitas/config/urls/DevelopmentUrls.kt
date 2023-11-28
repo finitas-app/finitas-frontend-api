ed86ba1b39e2c987b64efd42551bb1813564dd8d
unknown <ritalincheg@gmail.com>
2023-11-28 23:39:26 +0100

    feat: add receipt parse timeout

--- src/main/kotlin/com/finitas/config/urls/DevelopmentUrls.kt
+++ src/main/kotlin/com/finitas/config/urls/DevelopmentUrls.kt
@@ -2,4 +2,5 @@ package com.finitas.config.urls
 
 object DevelopmentUrls : UrlProvider {
     override val RECEIPT_SERVICE_HOST_URL = "http://localhost:8081/parse"
+    override val RECEIPT_PARSING_TIMEOUT = 8000L
 }
--- src/main/kotlin/com/finitas/config/urls/ProductionUrls.kt
+++ src/main/kotlin/com/finitas/config/urls/ProductionUrls.kt
@@ -2,4 +2,5 @@ package com.finitas.config.urls
 
 object ProductionUrls : UrlProvider {
     override val RECEIPT_SERVICE_HOST_URL = "http://localhost:8080/parse"
+    override val RECEIPT_PARSING_TIMEOUT = 1000L
 }
