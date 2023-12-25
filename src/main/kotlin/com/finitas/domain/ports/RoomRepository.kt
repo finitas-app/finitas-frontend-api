16abd487d8b17a331934ba54c68bd690a978b6d1
Danylo Hrebeniuk <dhr@touk.pl>
2023-12-25 23:34:36 +0100

    Add reachable users endpoint

--- src/main/kotlin/com/finitas/domain/ports/RoomRepository.kt
+++ src/main/kotlin/com/finitas/domain/ports/RoomRepository.kt
@@ -5,13 +5,21 @@ import com.finitas.domain.dto.store.CreateRoomDto
 import com.finitas.domain.dto.store.RoomDto
 import com.finitas.domain.dto.store.RoomVersionDto
 import kotlinx.serialization.Serializable
+import java.util.UUID
 
 interface RoomRepository {
 
     suspend fun createRoom(createRoomDto: CreateRoomDto): RoomDto
 
     suspend fun getRoomFromVersion(getRoomsFromVersionsDto: GetRoomsFromVersionsDto): List<RoomDto>
+
+    suspend fun getReachableUsersForUser(idUser: UUID, idRoom: UUID?): ReachableUsersDto
 }
+
+@Serializable
+data class ReachableUsersDto(
+    val reachableUsers: List<SerializableUUID>,
+)
 
 @Serializable
 data class GetRoomsFromVersionsDto(
