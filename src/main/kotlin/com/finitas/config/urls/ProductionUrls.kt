173ce6c11418ea4628ae2999b1a19e85f5a1d693
Danylo Hrebeniuk <dhr@touk.pl>
2024-01-09 01:03:52 +0100

    Increase prod receipt parse timeout

--- src/main/kotlin/com/finitas/config/urls/ProductionUrls.kt
+++ src/main/kotlin/com/finitas/config/urls/ProductionUrls.kt
@@ -6,7 +6,7 @@ object ProductionUrls : UrlProvider {
     override val AUTH0_FINITAS_API_AUDIENCE = "https://api.finitas.com"
     override val AUTH0_DOMAIN = "https://dev-ktqlmp7ts80r786t.us.auth0.com"
     override val RECEIPT_SERVICE_HOST_URL = "http://rp-service:8081/api/parse"
-    override val RECEIPT_PARSING_TIMEOUT = 8000L
+    override val RECEIPT_PARSING_TIMEOUT = 30000L
     override val STORE_HOST_URL = "http://fms-service:8082/api/store"
     override val ROOM_MANAGER_HOST_URL = "http://rm-service:8083/api"
 }
